6496f88a8255efdb3253ea9b6eca82574266ca80
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-09-02 13:25:00 -0400

    forgot to add a long description

--- setup.py
+++ setup.py
@@ -15,4 +15,20 @@ setup(
         ]
     },
     install_requires = ['konira>=0.1.0', 'pytest>=2.0'],
+    long_description = """
+
+A Pytest Plugin for Konira test cases
+-------------------------------------
+This plugin allows you to collect and execute Konira test cases as you would
+normally do with Konira's own test runner but with the extra functionality that
+comes with the awesome py.test
+
+Once the plugin is installed you get a single entry in the ``py.test`` help
+menu giving you the option to add the ``--konira`` flag to collect cases.
+
+Project page is at: http://github.com/alfredodeza/pytest-konira
+
+The actual Konira project can be found at: http://konira.cafepais.com
+
+"""
 )
