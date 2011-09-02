ea78d7bcb29f4d1ee6ea77b82aabfc18b366b7f9
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-09-02 13:11:36 -0400

    adding proper setup.py identifiers

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name             = 'pytest-konira',
-    description      = 'Run Konira DSL tests',
+    description      = 'Run Konira DSL tests with py.test',
     author           = 'Alfredo Deza',
     author_email     = 'alfredodeza [at] gmail.com',
     version          = '0.1',
+    license          = "MIT",
+    keywords         = "test, readable, testrunner, bdd",
     py_modules       = ['pytest_konira'],
     entry_points     = {
         'pytest11': [
